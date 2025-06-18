4aa9405ae1cb774b9e7d5aadea8c8fe8bf28cf07
starnowski <33316705+starnowski@users.noreply.github.com>
2025-06-18 21:16:31 +0200

    Added link [skip ci]

--- gpc-spring-boot-3/src/test/groovy/com/github/starnowski/service/LocalGCPStorageItTest.groovy
+++ gpc-spring-boot-3/src/test/groovy/com/github/starnowski/service/LocalGCPStorageItTest.groovy
@@ -1,18 +1,10 @@
 package com.github.starnowski.service
 
-import com.github.starnowski.GoogleCloudStorageContainer
-import com.google.cloud.NoCredentials
 import com.google.cloud.storage.Blob
 import com.google.cloud.storage.BlobId
 import com.google.cloud.storage.BlobInfo
-import com.google.cloud.storage.BucketInfo
 import com.google.cloud.storage.Storage
-import com.google.cloud.storage.StorageOptions
 import com.google.cloud.storage.contrib.nio.testing.LocalStorageHelper
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
 
 import java.nio.file.Files
@@ -20,7 +12,10 @@ import java.nio.file.Path
 
 class LocalGCPStorageItTest extends Specification {
 
-
+    /**
+     * https://dev.to/diogomoreira/mocking-google-cloud-storage-blob-objects-517n
+     * @return
+     */
     def "should upload text file to gcp storage" () {
         given:
         Storage storage = LocalStorageHelper.getOptions().getService()
